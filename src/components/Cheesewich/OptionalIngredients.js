c60d747254a29d7e785350359143c8828c07cbb4
chunyukuo88 <alexgochenour@gmail.com>
2020-11-27 00:27:04 -0500

    Cleaned up OptionalIngredients.js

--- src/components/Cheesewich/OptionalIngredients.js
+++ src/components/Cheesewich/OptionalIngredients.js
@@ -2,23 +2,17 @@ import React from 'react';
 import Ingredient from './Ingredients/Ingredients.jsx';
 
 export const OptionalIngredients = (ingredientsObject) => {
-    const result = Object.keys(ingredientsObject)
+    return Object.keys(ingredientsObject)
         .map(ingredientKey => {
             return _buildArrayOfSingleIngredient(ingredientsObject, ingredientKey);
         });
-    console.log('result: ', result);
-    return result;
 };
 
 export const _buildArrayOfSingleIngredient = (ingredients, key) => {
-    const result = [...Array(ingredients[key])]
+    return [...Array(ingredients[key])]
         .map((_, i) => {
             return _buildIngredient(key, i);
         });
-    return result;
 };
 
-export const _buildIngredient = (key, i) => {
-    const result = <Ingredient key={key + i} type={key}/>;
-    return result;
-};
+export const _buildIngredient = (key, i) => <Ingredient key={key + i} type={key}/>;
