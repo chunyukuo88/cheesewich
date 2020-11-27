0d6b10076f60a107611af0375f78398abd6766ee
chunyukuo88 <alexgochenour@gmail.com>
2020-11-27 14:07:48 -0500

    WIP: Refactoring Ingredients.jsx

--- src/components/Cheesewich/Ingredients/Ingredients.jsx
+++ src/components/Cheesewich/Ingredients/Ingredients.jsx
@@ -6,37 +6,28 @@ class Ingredient extends Component {
     render() {
         let ingredient = null;
         switch (this.props.type) {
-            case ('bread-bottom'): 
-                ingredient = <div className={classes.BreadBottom}></div>;
-                break;
-            case ('bread-top'): 
-                ingredient = (
-                    <div className={classes.BreadTop}>
-                        <div className={classes.Seeds1}></div>
-                        <div className={classes.Seeds2}></div>
-                    </div>
-                );
-                break;
-            case ('meat'): 
-                ingredient = (
-                    <div className={classes.Meat}></div>
-                );
-                break;
-            case ('cheese'): 
-                ingredient = (
-                    <div className={classes.Cheese}></div>
-                );
-                break;
-            case ('salad'): 
-                ingredient = (
-                    <div className={classes.Salad}></div>
-                );
-                break;
+            case ('bread-bottom'): ingredient = BreadBottom(); break;
+            case ('bread-top'): ingredient = BreadTop(); break;
+            case ('meat'): ingredient = Meat(); break;
+            case ('cheese'): ingredient = Cheese(); break;
+            case ('salad'): ingredient = Salad(); break;
             default: ingredient = null;
         }
         return ingredient;
     }
 };
+
+const BreadTop = () => (
+    <div className={classes.BreadTop}>
+        <div className={classes.Seeds1}></div>
+        <div className={classes.Seeds2}></div>
+    </div>
+);   
+
+const Meat = () => <div className={classes.Meat}></div>;
+const BreadBottom = () => <div className={classes.BreadBottom}></div>
+const Cheese = () => <div className={classes.Cheese}></div>;
+const Salad = () => <div className={classes.Salad}></div>;
 
 Ingredient.propTypes = {
     type: PropTypes.string.isRequired
