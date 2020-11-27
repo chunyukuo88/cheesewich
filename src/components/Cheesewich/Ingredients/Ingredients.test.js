e15aedeeb6aa1a5620038ace9bc0c72ea2c767a0
chunyukuo88 <alexgochenour@gmail.com>
2020-11-27 15:48:43 -0500

    I think I need to mock out the _buildIngredientDisplay function.

--- src/components/Cheesewich/Ingredients/Ingredients.test.js
+++ src/components/Cheesewich/Ingredients/Ingredients.test.js
@@ -1,28 +1,27 @@
 import React from 'react';
+import { findByTestAttr } from '../../../testUtils.js';
 import Ingredient from './Ingredients.jsx';
-import { findByTestAttr } from '../../../testUtils';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 
 const props = {
     type: 'cheese'
 };
-const setup = () => shallow(<Ingredient {...props}/>);
 
 describe('Ingredient.jsx', ()=>{
     describe('Ingredient()', ()=>{
         test('Renders without crashing.', ()=>{
-            const wrapper = setup();
+            const wrapper = shallow(<Ingredient {...props}/>);
             expect(wrapper.length).toBe(1);
         });
     });
     describe('_buildIngredientDisplay()', ()=>{
         describe('WHEN: given a valid (string) prop, ', ()=>{
         test('THEN: It returns an ingredient corresponding to that string.', ()=>{
-            const wrapper = setup();
-            const ingredient = findByTestAttr(wrapper, 'ingredient');
-            expect(ingredient.hasClass('cheese')).toBe(true);
+            //TODO: mock out the _buildIngredientDisplay function.
+            const wrapper = shallow(<Ingredient {...props}/>);
+            expect(wrapper.hasClass('cheese')).toBe(true);
         });
     });
 });
