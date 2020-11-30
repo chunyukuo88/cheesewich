3635ac53657558a10bc9975faa93184661fa7d0f
chunyukuo88 <alexgochenour@gmail.com>
2020-11-29 23:53:20 -0500

    Fixed the Backdrop css, which was missing the rgba from the background-color rule.

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import classes from './Modal.css';
 import { _modalStyle } from './modalUtils';
+import Backdrop from '../Backdrop/Backdrop.jsx';
+import Aux from '../../../hoc/auxilliary.js';
 
 const Modal = props => (
-    <div className={classes.Modal} 
-         style={_modalStyle(props.show)}>
-        {props.children}
-    </div>
+    <Aux>
+        <Backdrop show={props.show}/>
+        <div className={classes.Modal} 
+            style={_modalStyle(props.show)}>
+            {props.children}
+        </div>
+    </Aux>
 );
 
 export default Modal;
