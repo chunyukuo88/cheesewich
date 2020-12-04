5b98477d51a01ce50cb9965d805043942d3b64bf
chunyukuo88 <alexgochenour@gmail.com>
2020-12-03 23:31:57 -0500

    Added lifestyle hooks to OrderSummary and Modal to improve performance by preventing unnecessary rerenders.

--- src/components/Cheesewich/OrderSummary/OrderSummary.jsx
+++ src/components/Cheesewich/OrderSummary/OrderSummary.jsx
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/auxilliary';
 import * as utils from './orderSummaryUtils';
 import Button from '../../UI/Button/Button.jsx';
 
-const OrderSummary = props => {
-    return (
-        <Aux>
-            <h3>Your Order:</h3>
-            <p>A scrumptious cheesewich with the following ingredients:</p>
-            <ul>
-                {ingredientSummary(props.ingredients)}
-            </ul>
-            <p>Total price: {props.price.toFixed(2)}</p>
-            <p>Proceed to checkout?</p>
-            <Button buttonType="green"
-                    clicked={props.goToCheckout}>Yep!</Button>
-            <Button buttonType="red"
-                    clicked={props.orderCancelled}>Nope!</Button>
-        </Aux>
-    );
+class OrderSummary extends Component {
+
+    componentDidUpdate() {
+        console.log('Summary did update');
+    }
+
+    render(){
+        return (
+            <Aux>
+                <h3>Your Order:</h3>
+                <p>A scrumptious cheesewich with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary(this.props.ingredients)}
+                </ul>
+                <p>Total price: {this.props.price.toFixed(2)}</p>
+                <p>Proceed to checkout?</p>
+                <Button buttonType="green"
+                        clicked={this.props.goToCheckout}>Yep!</Button>
+                <Button buttonType="red"
+                        clicked={this.props.orderCancelled}>Nope!</Button>
+            </Aux>
+        );
+    }
 };
 
 const ingredientSummary = ingredients => utils._getIngredientSummary(ingredients);
--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import { _modalStyle } from './modalUtils';
 import Backdrop from '../Backdrop/Backdrop.jsx';
 import Aux from '../../../hoc/auxilliary.js';
 
-const Modal = props => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div className={classes.Modal} 
-            style={_modalStyle(props.show)}>
-            {props.children}
-        </div>
-    </Aux>
-);
+class Modal extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    componentDidUpdate() {
+        console.log('Modal was updated');
+    }
+
+    render(){
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div className={classes.Modal} 
+                    style={_modalStyle(this.props.show)}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        );
+    };
+}
 
 export default Modal;
