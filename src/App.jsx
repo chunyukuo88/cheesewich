e600043444cf4503f2e9f5477a7c7bbac0db5eff
chunyukuo88 <alexgochenour@gmail.com>
2020-12-08 16:59:52 -0500

    Began adding routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout.jsx';
-import Cheesewich from './containers/CheesewichBuilder/CheesewichBuilder.jsx';
+import CheesewichBuilder from './containers/CheesewichBuilder/CheesewichBuilder.jsx';
+import Checkout from './containers/Checkout/Checkout';
 
 const App = () => (
   <div>
     <Layout>
-        <Cheesewich/>
+        <Switch>
+            <Route path='/' component={CheesewichBuilder}/>
+            <Route path='/checkout' component={Checkout}/>
+        </Switch>
     </Layout>
   </div>
 );
