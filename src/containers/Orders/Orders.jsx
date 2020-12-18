b917a95039f551b8135d9ead520a117781e72da4
chunyukuo88 <alexgochenour@gmail.com>
2020-12-18 15:22:44 -0500

    Orders component now fetches orders from the back end.

--- src/containers/Orders/Orders.jsx
+++ src/containers/Orders/Orders.jsx
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
-    render() {
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchedOrders = this.buildFetchedOrders(res);
+                this.setState({ orders: fetchedOrders, loading: false});
+                console.log(this.state.orders);
+            })
+            .catch(err => {
+                this.setState({ loading: false});
+            });
+    }
+
+    buildFetchedOrders = (serverResponseObject) => {
+        const fetchedOrders = [];
+        for(const key in serverResponseObject.data){
+            fetchedOrders.push({
+                ...serverResponseObject.data[key],
+                id: key
+            });
+        };
+        return fetchedOrders;
+    }
+
+    render(){
         return (
             <div>
-                <Order />
-                <Order />
+
             </div>
         );
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
