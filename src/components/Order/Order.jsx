2332195eb68ea662a81bb2da6048484523728f2d
chunyukuo88 <alexgochenour@gmail.com>
2020-12-18 23:19:54 -0500

    More refactoring.

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import classes from './Order.css';
 
-const Order = ({ ingredients, price }) => {
+const Order = ({ ingredients, price }) => (
+  <div className={classes.Order}>
+      <p>Ingredients:</p>
+      <ul>
+          {ingredientsDisplay(ingredients)}
+      </ul>
+      <p>Price: <strong>${price}</strong></p>
+  </div>
+);
+
+const ingredientsDisplay = (ingredients) => {
     const ingredientArray = [];
-
     for (const ingredient in ingredients) {
-        ingredientArray.push({name: ingredient, amount: ingredients[ingredient]});
-    }
-
-    const ingredientDisplay = ingredientArray.map(ing => {
-        return <li>{ing.name}: {ing.amount}</li>;
-    })
-
-    return (
-      <div className={classes.Order}>
-          <p>Ingredients:</p>
-          <ul>
-              {ingredientDisplay}
-          </ul>
-          <p>Price: <strong>${price}</strong></p>
-      </div>
-    );
-}
+        ingredientArray.push({
+            name: ingredient,
+            amount: ingredients[ingredient]
+        });
+    };
+    return ingredientArray.map(({name, amount}) => <li>{name}: {amount}</li>);
+};
 
 export default Order;
