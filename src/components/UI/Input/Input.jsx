63f3b2203cf8ea2fb7efdc8e46ed7850f8300db1
chunyukuo88 <alexgochenour@gmail.com>
2020-12-20 17:54:30 -0500

    Delivery options now appear.

--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -13,15 +13,24 @@ const Input = props => {
 const getInputElement = (props) => {
     let inputElement = null;
     switch (props.elementType) {
-        case ('textarea'): inputElement = <textarea className={classes.InputElement}
-                                                    value={props.value}
-                                                    {...props.elementType}/>; break;
-        case ('input'): inputElement = <input className={classes.Input}
-                                              value={props.value}
-                                              {...props.elementType}/>; break;
+        case ('textarea'):
+            inputElement = <textarea className={classes.InputElement} value={props.value} {...props.elementConfig}/>;
+            break;
+        case ('input'):
+            inputElement = <input className={classes.Input} value={props.value} {...props.elementConfig}/>;
+            break;
+        case ('select'):
+            inputElement = <select className={classes.Input} value={props.value} {...props.elementConfig}>
+                                {props.elementConfig.options.map(option => (
+                                    <option value={option.value}>
+                                        {option.displayValue}
+                                    </option>
+                                ))}
+                           </select>;
+        break;
         default: inputElement = <input className={classes.Label}
                                        value={props.value}
-                                       {...props.elementType}/>; break;
+                                       {...props.elementConfig}/>; break;
     }
     return inputElement;
 }
