e9e22b8ccfa6586ec3707b4d0085ea7c00b8a824
chunyukuo88 <alexgochenour@gmail.com>
2020-12-21 00:18:15 -0500

    Refactoring.

--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -16,7 +16,10 @@ const getInputElement = (props) => {
     let inputElement = null;
     const inputClasses = [classes.InputElement];
     checkForInvalidity(props, inputClasses);
+    return buildInputElement(props, inputElement, inputClasses);
+}
 
+const buildInputElement = (props, inputElement, inputClasses) => {
     //TODO: Improve this after converting everything to classes AND adding Redux to the project.
     switch (props.elementType) {
         case ('textarea'):
@@ -24,35 +27,32 @@ const getInputElement = (props) => {
                                      onChange={props.changed}
                                      value={props.value}
                                      {...props.elementConfig}/>;
-            console.log(inputElement);
             break;
         case ('input'):
             inputElement = <input className={inputClasses.join(' ')}
                                   onChange={props.changed}
                                   value={props.value}
                                   {...props.elementConfig}/>;
-            console.log(inputElement);
             break;
         case ('select'):
             inputElement = <select className={inputClasses.join(' ')}
                                    onChange={props.changed}
                                    value={props.value}
                                    {...props.elementConfig}>
-                                {props.elementConfig.options.map(option => (
-                                    <option key={option.value} value={option.value}>
-                                        {option.displayValue}
-                                    </option>
-                                ))}
-                           </select>;
+                {props.elementConfig.options.map(option => (
+                    <option key={option.value} value={option.value}>
+                        {option.displayValue}
+                    </option>
+                ))}
+            </select>;
             break;
         default:
             inputElement = <input className={classes.Label}
-                                       value={props.value}
-                                       {...props.elementConfig}/>;
-            console.log(inputElement);
+                                  value={props.value}
+                                  {...props.elementConfig}/>;
             break;
     }
     return inputElement;
-}
+};
 
 export default Input;
