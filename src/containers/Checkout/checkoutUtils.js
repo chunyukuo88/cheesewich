d4818cad5e879dbba789489bb1e5a22360d8f55b
chunyukuo88 <alexgochenour@gmail.com>
2020-12-24 14:52:59 -0500

    Refactored checkoutUtils

--- src/containers/Checkout/checkoutUtils.js
+++ src/containers/Checkout/checkoutUtils.js
@@ -1,13 +1,18 @@
 export const getIngredientsFromURI = props => {
     const ingredients = {};
     let price = 0;
+    console.log('props.location: ', props.location);
     const query = new URLSearchParams(props.location.search);
+    extractPriceAndIngredients(ingredients, price, query);
+    return { ingredients, price };
+}
+
+const extractPriceAndIngredients = (ingredients, price, query) => {
     for (const param of query.entries()) {
         if (param[0] === 'price') {
             price = param[1];
         } else {
             ingredients[param[0]] = +param[1];
         }
-    };
-    return { ingredients, price };
+    };   
 }
