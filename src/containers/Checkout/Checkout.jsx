060c98ecc5efeff9628c93e6aadf379cdcf2268d
chunyukuo88 <alexgochenour@gmail.com>
2020-12-31 16:32:12 -0500

    Fixed a typo in the mapStateToProps function that was causing ingredients to be undefined.

--- src/containers/Checkout/Checkout.jsx
+++ src/containers/Checkout/Checkout.jsx
@@ -1,31 +1,36 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
 import { connect } from 'react-redux';
 
 class Checkout extends Component {
-
     checkoutCancelledHandler = () => this.props.history.goBack();
     checkoutContinueHandler = () => this.props.history.replace('/checkout/contact-data');
 
     render(){
-        return (
-            <div>
-                <CheckoutSummary ingredients={this.props.ings}
-                                 checkoutCancelled={this.checkoutCancelledHandler}
-                                 checkoutContinue={this.checkoutContinueHandler}/>
-                <Route
-                    path={`${this.props.match.path}/contact-data`}
-                    component={ContactData} />
-            </div>
-        );
-    }
+        const summary = getSummaryIfIngredientsExist(this.props, this.checkoutCancelledHandler, this.checkoutContinueHandler);
+        return summary;
+    };
 }
+
+const getSummaryIfIngredientsExist = (props, checkoutCancelFn, checkoutContinueFn) => {
+    return props.ingredients ? buildSummary(props, checkoutCancelFn, checkoutContinueFn) : <Redirect to="/"/>;
+};
+
+const buildSummary = (props, checkoutCancelFn, checkoutContinueFn) => (
+    <div>
+        <CheckoutSummary
+            ingredients={props.ingredients}
+            checkoutCancelled={checkoutCancelFn}
+            checkoutContinue={checkoutContinueFn}/>
+        <Route path={`${props.match.path}/contact-data`} component={ContactData} />
+    </div>
+);
 
 const mapStateToProps = (state) => {
     return {
-        ings: state.ingredients
+        ingredients: state.ingredients
     };
 };
 
