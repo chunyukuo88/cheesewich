0c5770b214478739f3bd1672ef3d2d3066af33d0
chunyukuo88 <alexgochenour@gmail.com>
2021-01-04 15:06:19 -0500

    Finished uno reversing the utils.js module. Now the helper functions are back in the reducers.

--- src/store/reducers/cheesewichBuilder.js
+++ src/store/reducers/cheesewichBuilder.js
@@ -2,7 +2,7 @@ import { ADD_INGREDIENT,
         NIX_INGREDIENT,
         SET_INGREDIENTS,
         FETCH_INGREDIENTS_FAILED } from '../actions/actionTypes';
-import * as utils from '../utils';
+import { updateObject } from '../utils';
 
 const initialState = {
     ingredients: null,
@@ -19,12 +19,43 @@ const INGREDIENT_PRICES = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case ADD_INGREDIENT: return utils.addIngredient(state, action, INGREDIENT_PRICES);
-        case NIX_INGREDIENT: return utils.nixIngredient(state, action, INGREDIENT_PRICES);
-        case SET_INGREDIENTS: return utils.setIngredients(state, action, initialState);
-        case FETCH_INGREDIENTS_FAILED: return utils.updateObject(state, {error: true});
+        case ADD_INGREDIENT: return addIngredient(state, action, INGREDIENT_PRICES);
+        case NIX_INGREDIENT: return nixIngredient(state, action, INGREDIENT_PRICES);
+        case SET_INGREDIENTS: return setIngredients(state, action, initialState);
+        case FETCH_INGREDIENTS_FAILED: return updateObject(state, {error: true});
         default: return state;
     }
+};
+
+export const addIngredient = (state, action, INGREDIENT_PRICES) => {
+    return {
+        ...state,
+        ingredients: {
+            ...state.ingredients,
+            [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+        },
+        price: state.price + INGREDIENT_PRICES[action.ingredientName],
+    };
+};
+
+export const nixIngredient = (state, action, INGREDIENT_PRICES) => {
+    return {
+        ...state,
+        ingredients: {
+            ...state.ingredients,
+            [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
+        },
+        price: state.price - INGREDIENT_PRICES[action.ingredientName],
+    };
+};
+
+export const setIngredients = (state, action, initialState) => {
+    const updatedProperties = {
+        ingredients: action.ingredients,
+        error: false,
+        price: initialState.price
+    };
+    return updateObject(state, updatedProperties);
 };
 
 export default reducer;
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,5 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
-import { updateObject, purchaseCheesewichSuccess } from '../utils';
+import { updateObject } from '../utils';
 
 const initialState = {
     orders: [],
@@ -24,6 +24,19 @@ const reducer = (state = initialState, action) => {
         case actionTypes.FETCH_ORDERS_FAIL:
             return updateObject(state, {loading: false});
         default: return state;
+    };
+};
+
+const purchaseCheesewichSuccess = (state, action) => {
+    const  newOrder = {
+        ...action.orderData,
+        id: action.orderId,
+    };
+    return {
+        ...state,
+        loading: false,
+        orders: state.orders.concat(newOrder),
+        purchaseHasBeenMade: true,
     };
 };
 
