f322dd483e38fe412b09f52f7ec0655a4313d86a
chunyukuo88 <alexgochenour@gmail.com>
2021-01-03 21:24:12 -0500

    Housekeeping.

--- src/components/Cheesewich/OrderSummary/OrderSummary.jsx
+++ src/components/Cheesewich/OrderSummary/OrderSummary.jsx
@@ -1,27 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Aux from '../../../hoc/auxilliary';
 import * as utils from './orderSummaryUtils';
 import Button from '../../UI/Button/Button.jsx';
 
-class OrderSummary extends Component {
-    render(){
-        return (
-            <Aux>
-                <h3>Your Order:</h3>
-                <p>A scrumptious cheesewich with the following ingredients:</p>
-                <ul>
-                    {ingredientSummary(this.props.ingredients)}
-                </ul>
-                <p>Total price: {this.props.price.toFixed(2)}</p>
-                <p>Proceed to checkout?</p>
-                <Button buttonType="green"
-                        clicked={this.props.goToCheckout}>Yep!</Button>
-                <Button buttonType="red"
-                        clicked={this.props.orderCancelled}>Nope!</Button>
-            </Aux>
-        );
-    }
-};
+const OrderSummary = (props) => (
+    <Aux>
+        <h3>Your Order:</h3>
+        <p>A scrumptious cheesewich with the following ingredients:</p>
+        <ul>
+            {ingredientSummary(props.ingredients)}
+        </ul>
+        <p>Total price: {props.price.toFixed(2)}</p>
+        <p>Proceed to checkout?</p>
+        <Button buttonType="green" clicked={props.goToCheckout}>Yep!</Button>
+        <Button buttonType="red" clicked={props.orderCancelled}>Nope!</Button>
+    </Aux>
+);
 
 const ingredientSummary = ingredients => utils._getIngredientSummary(ingredients);
 
