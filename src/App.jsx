3eb8266864be131597edbf1e84212d484e2c07db
chunyukuo88 <alexgochenour@gmail.com>
2021-01-06 14:44:43 -0500

    Trying to get local storage to work.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout.jsx';
 import CheesewichBuilder from './containers/CheesewichBuilder/CheesewichBuilder.jsx';
@@ -6,17 +6,34 @@ import Checkout from './containers/Checkout/Checkout.jsx';
 import Orders from './containers/Orders/Orders.jsx';
 import Auth from './containers/Auth/Auth.jsx';
 import Logout from './containers/Auth/Logout/Logout';
+import { connect } from 'react-redux';
+import { authCheckState } from './store/actions/auth';
 
-const App = () => (
-    <Layout>
-        <Switch>
-            <Route path='/' exact component={CheesewichBuilder}/>
-            <Route path='/auth' component={Auth}/>
-            <Route path='/checkout' component={Checkout}/>
-            <Route path='/orders' component={Orders}/>
-            <Route path='/logout' component={Logout}/>
-        </Switch>
-    </Layout>
-);
+class App extends Component {
 
-export default App;
+    componentDidMount() {
+        this.props.onTryAutoSignup();
+    }
+
+    render() {
+        return (
+            <Layout>
+                <Switch>
+                    <Route path='/' exact component={CheesewichBuilder}/>
+                    <Route path='/auth' component={Auth}/>
+                    <Route path='/checkout' component={Checkout}/>
+                    <Route path='/orders' component={Orders}/>
+                    <Route path='/logout' component={Logout}/>
+                </Switch>
+            </Layout>
+        );
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onTryAutoSignup: () => dispatch(authCheckState()),
+    };
+};
+
+export default connect(null, mapDispatchToProps)(App);
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -2,17 +2,42 @@ import axios from 'axios';
 import * as actionTypes from './actionTypes';
 import urls from '../../urls';
 
-export const authStart = () => {
-    return {
-        type: actionTypes.AUTH_START,
+export const auth = (email, password, isSignup) => {
+    return dispatch => {
+        dispatch(authStart());
+        const authData = buildAuthData(email, password);
+        const url = getAuthUrl(isSignup);
+        axios.post(url, authData)
+            .then(response => {
+                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('expirationDate', expirationDate);
+                localStorage.setItem('userId', response.data.localId);
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
+            })
+            .catch(err => {
+                dispatch(authFail(err.message));
+            });
     };
 };
 
-export const authSuccess = ({ idToken, userId }) => {
-    return {
-        type: actionTypes.AUTH_SUCCESS,
-        idToken: idToken,
-        userId: userId
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            return;
+        } else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+            if (expirationDate > new Date()) {
+                dispatch(logout());
+            } else {
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()));
+            };
+        };
+        return token;
     };
 };
 
@@ -23,11 +48,25 @@ export const authFail = (error) => {
     };
 };
 
-export const logout = () => {
+export const authStart = () => {
     return {
-        type: actionTypes.AUTH_LOGOUT,
-        idToken: null,
-        userId: null
+        type: actionTypes.AUTH_START,
+    };
+};
+
+export const authSuccess = (idToken, userId) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        idToken: idToken,
+        userId: userId
+    };
+};
+
+const buildAuthData = (email, password) => {
+    return {
+        email: email,
+        password: password,
+        returnSecureToken: true,
     };
 };
 
@@ -39,19 +78,16 @@ export const checkAuthTimeout = (expirationTime) => {
     };
 };
 
-export const auth = (email, password, isSignup) => {
-    return dispatch => {
-        dispatch(authStart());
-        const authData = buildAuthData(email, password);
-        const url = getAuthUrl(isSignup);
-        axios.post(url, authData)
-            .then(response => {
-                dispatch(authSuccess(response.data));
-                dispatch(checkAuthTimeout(response.data.expiresIn));
-            })
-            .catch(err => {
-                dispatch(authFail(err.message));
-            });
+
+const getAuthUrl = (isSignup) => (isSignup) ? urls.authSignIn : urls.authSignUp;
+
+export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    return {
+        type: actionTypes.AUTH_LOGOUT,
+        idToken: null,
+        userId: null
     };
 };
 
@@ -61,13 +97,3 @@ export const setAuthRedirectPath = (path) => {
         path: path,
     };
 };
-
-const getAuthUrl = (isSignup) => (isSignup) ? urls.authSignIn : urls.authSignUp;
-
-const buildAuthData = (email, password) => {
-    return {
-        email: email,
-        password: password,
-        returnSecureToken: true,
-    };
-}
