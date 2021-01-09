d8740de086c9c040727d9195f2746c718e5cd79d
chunyukuo88 <alexgochenour@gmail.com>
2021-01-09 00:30:16 -0500

    Side drawer closes when you click anywhere on it.

--- src/components/Navigation/SideDrawer/SideDrawer.jsx
+++ src/components/Navigation/SideDrawer/SideDrawer.jsx
@@ -1,29 +1,28 @@
 import React from 'react';
 //TODO: import the logo
-import NavItems from '../NavItems/NavItems.jsx';
 import classes from './SideDrawer.css';
-import Backdrop from '../../UI/Backdrop/Backdrop.jsx';
 import Aux from '../../../hoc/auxilliary.js';
+import Backdrop from '../../UI/Backdrop/Backdrop.jsx';
+import NavItems from '../NavItems/NavItems.jsx';
 
-const SideDrawer = props => {
-    return (
-        <Aux>
-            <Backdrop show={props.open} clicked={props.closed}/>
-            <div className={getClasses(props).join(' ')}>
-                {/* Logo */}
-                <nav>
-                    <NavItems isAuthenticated={props.isAuth} />
-                </nav>
-            </div>
-        </Aux>
-    );
-}
+
+
+const SideDrawer = props => (
+    <Aux>
+        <Backdrop show={props.open} clicked={props.closed}/>
+        <div className={getClasses(props).join(' ')} onClick={props.closed}>
+            {/* Logo */}
+            <nav>
+                <NavItems isAuthenticated={props.isAuth} />
+            </nav>
+        </div>
+    </Aux>
+);
 
 export const getClasses = (props) => {
     return props.open
         ? [classes.side_drawer, classes.open]
         : [classes.side_drawer, classes.closed];
 };
-
 
 export default SideDrawer;
