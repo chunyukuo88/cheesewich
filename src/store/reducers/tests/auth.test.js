725fc578d4095185ab9dca44cf95ba097bad8401
chunyukuo88 <alexgochenour@gmail.com>
2021-01-11 10:28:17 -0500

    100% coverage for auth reducer.

---
+++ src/store/reducers/tests/auth.test.js
@@ -0,0 +1,98 @@
+import reducer from '../auth';
+import * as actionTypes from '../../actions/actionTypes';
+
+const initialState = {
+    token: null,
+    userId: null,
+    error: null,
+    loading: false,
+    authRedirectPath: '/',
+};
+
+describe('reducer()', ()=>{
+    describe('WHEN: Given no state and an invalid action,', ()=>{
+        test('THEN: It returns the state it was given.', ()=> {
+            const action = {};
+            const result = reducer(undefined, action);
+            expect(result).toEqual(initialState);
+        });
+    });
+    describe('WHEN: Given an action type of AUTH_START,', ()=>{
+        test('THEN: The state is update accordingly.', ()=>{
+            const action = { type: actionTypes.AUTH_START};
+            const result = reducer(initialState, action);
+            const expectedResult = {
+                token: null,
+                userId: null,
+                error: null,
+                loading: true,
+                authRedirectPath: '/',
+            };
+            expect(result).toEqual(expectedResult);
+        });
+    });
+    describe('WHEN: Given an action type of AUTH_SUCCESS,', ()=>{
+        test('THEN: The state is update accordingly.', ()=>{
+            const action = {
+                type: actionTypes.AUTH_SUCCESS,
+                idToken: {
+                    foo: 'bar'
+                },
+                userId: 456,
+            };
+            const result = reducer(initialState, action);
+            const expectedResult = {
+                token: action.idToken,
+                userId: action.userId,
+                error: null,
+                loading: false,
+                authRedirectPath: '/',
+            };
+            expect(result).toEqual(expectedResult);
+        });
+    });
+    describe('WHEN: Given an action type of AUTH_FAIL,', ()=>{
+        test('THEN: The state is update accordingly.', ()=>{
+            const action = {
+                type: actionTypes.AUTH_FAIL,
+                error: 'error!!'
+            };
+            const result = reducer(initialState, action);
+            const expectedResult = {
+                token: null,
+                userId: null,
+                error: 'error!!',
+                loading: false,
+                authRedirectPath: '/',
+            };
+            expect(result).toEqual(expectedResult);
+        });
+    });
+    describe('WHEN: Given an action type of AUTH_LOGOUT,', ()=>{
+        test('THEN: The state is update accordingly.', ()=>{
+            const action = {
+                type: actionTypes.AUTH_LOGOUT,
+                userId: 234,
+            };
+            const result = reducer(initialState, action);
+            const expectedResult = {
+                userId: 234,
+                error: null,
+                loading: false,
+                authRedirectPath: '/',
+            };
+            expect(result).toEqual(expectedResult);
+        });
+    });
+    describe('WHEN: Given an action type of SET_AUTH_REDIRECT_PATH,', ()=>{
+        test('THEN: The state is update accordingly.', ()=>{
+            const action = {
+                type: actionTypes.SET_AUTH_REDIRECT_PATH,
+                path: '/test',
+            };
+            const result = reducer(initialState, action);
+            initialState.authRedirectPath = '/test';
+            expect(result).toEqual(initialState);
+        });
+    });
+});
