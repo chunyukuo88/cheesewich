167b90d28d3722439031ff6c2954007f4fb1feeb
chunyukuo88 <alexgochenour@gmail.com>
2021-01-12 18:49:13 -0500

    Adding yield and put to a push() call inside a for() block broke everything. The saga worked as soon as I removed the yield put from line 15 of sagas/order.js

--- src/store/sagas/order.js
+++ src/store/sagas/order.js
@@ -4,18 +4,18 @@ import * as actions from '../actions/order';
 
 
 
-export function* fetchOrdersSaga(token, userId){
-    yield put (actions.fetchOrdersStart());
+export function* fetchOrdersSaga(action){
+    yield put(actions.fetchOrdersStart());
     //The `orderBy` and `equalTo` query parameters are specific to this particular back end:
-    const queryParams = yield `?auth=${token}&orderBy="userId"&equalTo="${userId}"`;
+    const queryParams = yield `?auth=${action.token}&orderBy="userId"&equalTo="${action.userId}"`;
     try{
         const response = yield axios.get( `/orders.json${queryParams}` );
         const fetchedOrders = yield [];
         for (const key in response.data) {
-            yield put(fetchedOrders.push({
+            fetchedOrders.push({
                 ...response.data[key],
                 id: key
-            }));
+            });
         };
         yield put(actions.fetchOrdersSuccess(fetchedOrders));
     } catch (err) {
@@ -27,7 +27,6 @@ export function* purchaseCheesewichSaga(action){
     yield put(actions.purchaseCheesewichStart());
     try {
         const response = yield axios.post( `/orders.json?auth=${action.token}`, action.orderData );
-        //TODO: NOTE: the .name might not be necessary; check this.
         yield put(actions.purchaseCheesewichSuccess(response.data.name, action.orderData));
     } catch (error) {
         yield put(actions.purchaseCheesewichFailed(error));
