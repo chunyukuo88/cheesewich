7d85e9ef2b95939321e5ddda7e229f8bd647b0a8
chunyukuo88 <alexgochenour@gmail.com>
2021-01-13 17:24:23 -0500

    Cheesewich builder action creator is at 100% coverage. All async code has been moved to the saga.

--- src/store/sagas/cheesewichBuilder.js
+++ src/store/sagas/cheesewichBuilder.js
@@ -3,7 +3,6 @@ import { put } from 'redux-saga/effects';
 import urls from '../../urls';
 import { setFetchIngredientError, setIngredients } from '../actions/cheesewichBuilder';
 
-
 export function* initIngredientsSaga () {
     const response = yield axios.get(urls.firebaseIngredients);
     try {
