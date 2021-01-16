b9d604b90c18a563674bf1c53b4a172a63a47eca
chunyukuo88 <alexgochenour@gmail.com>
2021-01-15 21:44:13 -0500

    Refactored Modal component.

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,24 +1,28 @@
-import React, { Component } from 'react';
+import React, { memo } from 'react';
 import classes from './Modal.css';
-import { modalStyle, childrenOrShowHaveChanged } from './modalUtils';
 import Backdrop from '../Backdrop/Backdrop.jsx';
 import Aux from '../../../hoc/auxilliary.js';
 
-class Modal extends Component {
+const Modal = memo((props) => (
+    <Aux>
+        <Backdrop show={props.show} clicked={props.modalClosed}/>
+        <div className={classes.Modal}
+             style={modalStyle(props.show)}>
+            {props.children}
+        </div>
+    </Aux>
+));
 
-    shouldComponentUpdate = (nextProps, nextState) => childrenOrShowHaveChanged(nextProps, this.props);
+const modalStyle = show => show ? modalShowing : modalHidden;
 
-    render(){
-        return (
-            <Aux>
-                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
-                <div className={classes.Modal} 
-                    style={modalStyle(this.props.show)}>
-                    {this.props.children}
-                </div>
-            </Aux>
-        );
-    };
+const modalShowing = {
+    transform: 'translateY(0)',
+    opacity: '1'
+}
+
+const modalHidden = {
+    transform: 'translateY(-100vh)',
+    opacity: '0'
 }
 
 export default Modal;
