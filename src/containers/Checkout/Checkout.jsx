b78fef0579cc27bea9bfa83762a143d086add54f
chunyukuo88 <alexgochenour@gmail.com>
2021-01-18 22:06:48 -0500

    Checkout.jsx now uses the useSelector and useDispatch hooks instead of the connect() function.

--- src/containers/Checkout/Checkout.jsx
+++ src/containers/Checkout/Checkout.jsx
@@ -2,39 +2,35 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 
 const Checkout = (props) => {
     const checkoutCancelledHandler = () => props.history.goBack();
     const checkoutContinueHandler = () => props.history.replace('/checkout/contact-data');
-    const summary = getSummaryIfIngredientsExist(props, checkoutCancelledHandler, checkoutContinueHandler);
+    const ingredients = useSelector(state => state.builder.ingredients);
+    const purchased = useSelector(state => state.order.purchaseHasBeenMade);
+    const { path } = props.match;
+    const summary = getSummaryIfIngredientsExist(ingredients, purchased, path, checkoutCancelledHandler, checkoutContinueHandler);
     return summary;
 }
 
-const getSummaryIfIngredientsExist = (props, checkoutCancelFn, checkoutContinueFn) => props.ingredients
-    ? buildSummary(props, checkoutCancelFn, checkoutContinueFn)
+const getSummaryIfIngredientsExist = (ingredients, purchased, path, checkoutCancelFn, checkoutContinueFn) => ingredients
+    ? buildSummary(ingredients, purchased, path, checkoutCancelFn, checkoutContinueFn)
     : <Redirect to="/" />;
 
-const buildSummary = (props, checkoutCancelFn, checkoutContinueFn) => {
-    const redirectOnPurchase = props.purchased && <Redirect to="/"/>;
+const buildSummary = (ingredients, purchased, path, checkoutCancelFn, checkoutContinueFn) => {
+    const redirectOnPurchase = purchased && <Redirect to="/"/>;
     return (
         <div>
             {redirectOnPurchase}
             <CheckoutSummary
-                ingredients={props.ingredients}
+                ingredients={ingredients}
                 checkoutCancelled={checkoutCancelFn}
                 checkoutContinue={checkoutContinueFn}/>
-            <Route path={`${props.match.path}/contact-data`} component={ContactData} />
+            <Route path={`${path}/contact-data`} component={ContactData} />
         </div>
     );
-}
-
-const mapStateToProps = (state) => {
-    return {
-        ingredients: state.builder.ingredients,
-        purchased: state.order.purchaseHasBeenMade,
-    };
 };
 
-export default connect(mapStateToProps)(Checkout);
+export default Checkout;
