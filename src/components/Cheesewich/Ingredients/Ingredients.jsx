783fb78e84595c07be0352946c8e8d4a75d1cb47
chunyukuo88 <alexgochenour@gmail.com>
2021-02-01 18:00:05 -0500

    WIP: Nixed the isometric nonsense and went with 2D images instead.

--- src/components/Cheesewich/Ingredients/Ingredients.jsx
+++ src/components/Cheesewich/Ingredients/Ingredients.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './Ingredients.css';
 import PropTypes from 'prop-types';
+
 import cheese from './images/cheese.png';
 import crunchybits from './images/crunchybits.png';
 import olives from './images/olives.png';
@@ -8,18 +9,10 @@ import marinara from './images/marinara.png';
 
 const Ingredient = ({ type }) => {
     switch (type) {
-        case 'cheese': return <img className={classes.optional}
-                                   src={cheese}
-                                   alt="cheese"/>;
-        case 'crunchybits': return <img className={classes.optional}
-                                        src={crunchybits}
-                                        alt="crunchybits"/>;
-        case 'olives': return <img className={classes.optional}
-                                   src={olives}
-                                   alt="olives"/>;
-        case 'marinara': return <img className={classes.optional}
-                                     src={marinara}
-                                     alt="marinara"/>;
+        case 'cheese': return <img className={classes.optional} src={cheese} alt="cheese"/>;
+        case 'crunchybits': return <img className={classes.optional} src={crunchybits} alt="crunchybits"/>;
+        case 'olives': return <img className={classes.optional} src={olives} alt="olives"/>;
+        case 'marinara': return <img className={classes.optional} src={marinara} alt="marinara"/>;
         default: return null;
     }
 }
